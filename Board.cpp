c45e8ebfd14d6ef88f4b6a34b16fa62c8791c5ff
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-05 15:15:55 +0300

    Create Board.cpp

---
+++ Board.cpp
@@ -0,0 +1,30 @@
+#include "Board.hpp"
+#include <iostream>
+
+using namespace pandemic;
+
+
+namespace pandemic{
+       int& Board::operator[](City city)
+       {
+           return n_1;
+       }
+  
+       std::ostream& operator<< (std::ostream& os, const Board& b)
+       {
+           return os;
+       } 
+  
+       bool Board::is_clean()
+       {
+           return true;
+       }
+  
+       void Board::remove_cures()
+       {
+
+       }
+}
+
+
+
