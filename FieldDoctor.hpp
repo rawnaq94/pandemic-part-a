cf4396931e648f6fe409d6d9c97451af0a80d79e
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-05 15:54:51 +0300

    Create FieldDoctor.hpp

---
+++ FieldDoctor.hpp
@@ -0,0 +1,12 @@
+#include "Player.hpp"
+#include <iostream>
+
+namespace pandemic{
+    class FieldDoctor : public Player
+    {
+    
+            public:
+        FieldDoctor(Board board, City city);
+
+    };
+}
