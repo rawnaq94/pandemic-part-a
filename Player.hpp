35de8bdf937e7d6322f6ca393cceeed1899fb6c4
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-05 16:06:45 +0300

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -12,16 +12,16 @@ namespace pandemic
         
       public:
             Player();
-            Player(Board board,City c);
-            Player& drive(City c);
-            Player& fly_direct(City c);
-            Player& fly_charter(City c);
-            Player& fly_shuttle(City c);
+            Player(Board board,City city);
+            Player& drive(City city);
+            Player& fly_direct(City city);
+            Player& fly_charter(City city);
+            Player& fly_shuttle(City city);
             Player& build();
-            bool discover_cure(Color c);
-            Player& treat(City c);
+            bool discover_cure(Color city);
+            Player& treat(City city);
             std::string role();
-            Player& take_card(City c);
+            Player& take_card(City city);
     
 
     };
