c4aafb9eb4c5d60ab44a426232537834f4068d75
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2021-05-05 16:53:04 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,14 @@
+#include "doctest.h"
+#include "Board.hpp"
+#include "OperationsExpert.hpp"
+#include "Dispatcher.hpp"
+#include "Scientist.hpp"
+#include "Researcher.hpp"
+#include "Medic.hpp"
+#include "Virologist.hpp"
+#include "GeneSplicer.hpp"
+#include "FieldDoctor.hpp"
+using namespace std;
 
+
+using namespace pandemic;
